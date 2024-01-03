9f77a5120f04ce80fe95525671f17b80dacff53e
sadra-ghavami <sadra.ghavami@gmail.com>
2024-01-03 17:53:07 +0330

    cache_second_attempt

--- MEM/Cache.v
+++ MEM/Cache.v
@@ -5,7 +5,6 @@ module Cache(
 
     output reg ready
 );
-    wire [17:0] upper_addr, lower_addr;
 
     reg [63:0] way0_data [0:63];
     reg [63:0] way1_data [0:63];
@@ -15,7 +14,34 @@ module Cache(
     reg [9:0] tag_bit_way1 [0:63];
     reg [63:0] LRU;
 
-    assign upper_addr = {address[18:2] , 1'b0};
-    assign lower_addr = {address[18:2] , 1'b1};
+    wire [9:0] selected_tag_way0, selected_tag_way1, tag;
+    wire selected_valid_way0, selected_valid_way1;
+    wire [63:0] selected_data_way0, selected_data_way1;
+    wire [2:0] offset;
+    wire [5:0] index;
+    wire hit, start;
+    wire [31:0] selected_data;
+
+    Cache_hit_validation validator(
+        .tag_way0(selected_tag_way0), .tag_way1(selected_tag_way1), .tag_address(tag),
+        .valid_way0(selected_valid_way0), .valid_way1(selected_data_way1),
+        .data_way0(selected_data_way0), .data_way1(selected_data_way1),
+        .offset(offset),
+        .hit(hit),
+        .data_out(selected_data)
+    );
+
+
+    assign offset = address[2:0];
+    assign index = address[8:3];
+    assign tag = address[18:9];
+    assign selected_tag_way0 = tag_bit_way0[index];
+    assign selected_tag_way1 = tag_bit_way1[index];
+    assign selected_valid_way0 = valid_bit_way0[index];
+    assign selected_valid_way1 = valid_bit_way1[index];
+    assign selected_data_way0 = way0_data[index];
+    assign selected_data_way1 = way1_data[index];
+    assign start = (R_EN & ~hit) | W_EN;
+    
 
 endmodule
